1b13d6b06df43a86a224020b4aa13728ce602ea3
Nick Block <nicholasblock78@gmail.com>
2016-11-13 03:46:37 -0600

    save changes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,6 +5,24 @@ $(function() {
 	$('#dbc img').on('click', function() {
 		event.preventDefault();
 
-		$(this).parent().parent().append('July and August 2016')
+		$(this).parent().parent().append('summer 2016')
+	})
+
+	$('#choppingblock img').on('click', function() {
+		event.preventDefault();
+
+		$(this).parent().parent().append('Sept - Oct 2016')
+	})
+
+	$('#circus img').on('click', function() {
+		event.preventDefault();
+
+		$(this).parent().parent().append('summer 2015')
+	})
+
+	$('#pdhp img').on('click', function() {
+		event.preventDefault();
+
+		$(this).parent().parent().append('summers 2013 & 2014')
 	})
 })
